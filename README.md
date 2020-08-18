630e812943356cec1b0ae06b42f5502deaba9823
Rogerio Fontes <fontestz@gmail.com>
2020-08-17 21:31:34 -0300

    aula mvc 01

---
+++ README.md
@@ -0,0 +1,1 @@
+"# aula-arquitetura-unipac-mvc-" 
---
+++ src/main/java/br/com/unipac/mvcexample/controller/AlunoController.java
@@ -0,0 +1,20 @@
+package br.com.unipac.mvcexample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AlunoController {
+
+    @GetMapping("/ola")
+    public String ola(@RequestParam(name="nome", required = true,
+                                    defaultValue = "Rogerio") String nome,
+                                    Model model) {
+        model.addAttribute("nome", nome);
+        return "ola";
+    }
+
+
+}
